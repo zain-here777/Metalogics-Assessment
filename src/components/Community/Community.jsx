80f5419f1959ddc09dd1bacf384bbf881dc6ab15
zain-here777 <zainii.cs@gmail.com>
2026-01-15 00:24:59 +0500

    remove indent

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -13,19 +13,18 @@ const Community = () => {
                     <div className='flex-1'>1</div>
                     <div className='flex-1'>
                         <div className="m-auto w-full max-w-[680px] [&_h1]:text-[40px]! [&_h1]:leading-[55px]! [&_p]:text-[20px]! [&_p]:leading-[32px]!">
-                        <span className="tagline mb-[20px]">
-                            <FontAwesomeIcon icon="fa-solid fa-music" />
-                            What Sets Us Apart
-                        </span>
-                        <div className="banner-tittle">
-                            <h1>Inspiring <span>Musicians</span>.<br></br> Building Community.<br></br> Since 2017.</h1>
-                            <p className="break-keep">
-                               At Freeport Music Studio, we believe in nurturing not just musicians—but confident, creative individuals. Since 2017, we’ve been trusted by local families to deliver quality music education in a supportive, inspiring environment.
-                            </p>
+                            <span className="tagline mb-[20px]">
+                                <FontAwesomeIcon icon="fa-solid fa-music" />
+                                What Sets Us Apart
+                            </span>
+                            <div className="banner-tittle">
+                                <h1>Inspiring <span>Musicians</span>.<br></br> Building Community.<br></br> Since 2017.</h1>
+                                <p className="break-keep">
+                                At Freeport Music Studio, we believe in nurturing not just musicians—but confident, creative individuals. Since 2017, we’ve been trusted by local families to deliver quality music education in a supportive, inspiring environment.
+                                </p>
+                            </div>
                         </div>
                     </div>
-                    </div>
-
                 </div>
             </div>
         </div>
